1331eee7cc870b2166761554e765af052f8f30f3
anadevti <anacarolayne777@gmail.com>
2025-07-23 23:49:19 -0300

    att fork

--- src/CharpEvolution/Program.cs
+++ src/CharpEvolution/Program.cs
@@ -6,7 +6,7 @@ namespace CsharpEvolution
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Aninha!");
         }
     }
 }
