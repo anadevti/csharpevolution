61d18ee85dee5f6d3349655ba254d27858981294
anadevti <anacarolayne777@gmail.com>
2025-08-03 22:44:24 -0300

    feat: stoping loops

---
+++ src/CsharpEvolution.WeeklyChallenges/Weekly01/TestsStopLoops/StopLoop.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CsharpEvolution.WeeklyChallenges.Weekly01.TestsStopLoops;
+
+public class StopLoop
+{
+    public void TestBreak()
+    {
+        int[] numbers = { 1, 2, -3, 4, 0, 5 };
+        foreach (int n in numbers)
+        {
+            if (n < 0)
+                continue;
+            if (n == 0)
+                break;
+            Console.WriteLine($"Número positivo: {n}");
+        }
+    }
+
+    public void TestReturn()
+    {
+        int[] numbers = { 1, 2, -3, 4, 0, 5 };
+        foreach (int n in numbers)
+        {
+            if (n < 0)
+                continue;
+            if (n == 0)
+                return; // Sai do método
+            Console.WriteLine($"Número positivo: {n}");
+        }
+        Console.WriteLine("Loop terminado.");
+    }
+
+}
