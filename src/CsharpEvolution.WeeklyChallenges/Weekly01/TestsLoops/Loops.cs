18c50ac35eaeffa60f071f6f7c81dce4d18f2b27
anadevti <anacarolayne777@gmail.com>
2025-08-03 22:33:21 -0300

    feat: adding loops examples

---
+++ src/CsharpEvolution.WeeklyChallenges/Weekly01/TestsLoops/Loops.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace CsharpEvolution.WeeklyChallenges.Weekly01.TestsLoops;
+
+public class Loops
+{
+    // Loop For 
+    public void ForLoopExample()
+    {        // inicializacao, condição e incremento
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"For Loop Iteration: {i}");
+        }
+    }
+    
+    public void WhileLoopExample()
+    {
+        int i = 0; // inicializacao
+        while (i < 10) // condição
+        {
+            Console.WriteLine($"While Loop Iteration: {i}");
+            i++; // incremento
+        }
+    }
+    
+    public void DoWhileLoopExample()
+    {
+        int i = 0; // inicializacao
+        do // executa pelo menos uma vez
+        {
+            Console.WriteLine($"Do While Loop Iteration: {i}");
+            i++; // incremento
+        } while (i < 10); // condição
+    }
+    
+    public void forEachLoopExample()
+    {
+        string[] fruits = { "Apple", "Banana", "Cherry" }; // array de frutas
+        foreach (string fruit in fruits) // itera sobre cada fruta
+        {
+            Console.WriteLine($"Fruit: {fruit}");
+        }
+    }
+}
