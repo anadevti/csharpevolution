c4ed2f737915ae0a856755987f4622f495e1e624
anadevti <anacarolayne777@gmail.com>
2025-08-04 23:56:13 -0300

    feat: adding challanges

---
+++ Challenges/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using EnterpriseFinance = Enterprise.finance.Relatory;
+using EnterpriseRh = Enterprise.rh.Relatory;
+
+namespace enterprise.employees
+{
+    class Employee
+    {
+        public void TestEmployee()
+        {
+            Console.WriteLine("Olá, sou um funcionário!");
+        }
+    }
+}
+
+namespace Enterprise.finance
+{
+    class Relatory
+    {
+        public void TestMethodFinance()
+        {
+            Console.WriteLine("Olá, sou do Financeiro");
+        }
+    }
+}
+
+namespace Enterprise.rh
+{
+    class Relatory
+    {
+        public void TestMethodRh()
+        {
+            Console.WriteLine("Olá, sou do Rh!");
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        EnterpriseFinance enterpriseFinance = new EnterpriseFinance();
+        EnterpriseRh enterpriseRh = new EnterpriseRh();
+        Console.WriteLine($"Teste instanciacao");
+    }
+}
