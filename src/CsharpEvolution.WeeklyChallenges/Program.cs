aba2a264d92d4cbcad1c7c8e442b48a1da3b1b7e
anadevti <anacarolayne777@gmail.com>
2025-09-21 17:15:22 -0300

    exceptions final

--- src/CsharpEvolution.WeeklyChallenges/Program.cs
+++ src/CsharpEvolution.WeeklyChallenges/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq.Expressions;
 using CsharpEvolution.WeeklyChallenges.Weekly01.TestsStructs;
 using CsharpEvolution.WeeklyChallenges.Weekly01.Tests_Types;
 using CsharpEvolution.WeeklyChallenges.Weekly01.TestsOperadores;
@@ -7,49 +8,16 @@ using CsharpEvolution.WeeklyChallenges.Weekly01.TestsDestructors;
 using CsharpEvolution.WeeklyChallenges.Weekly01.TestsConditionals;
 using CsharpEvolution.WeeklyChallenges.Weekly01.ChallengeWeekly01;
 using CsharpEvolution.WeeklyChallenges.Weekly01.ChallangeWeekly01_2x;
+using CsharpEvolution.WeeklyChallenges.Weekly02.Exceptions_Tests;
+
 namespace CsharpEvolution.WeeklyChallenges
 {
     class Program
     {
         static void Main(string[] args)
         {
-           // ReferenceType referenceType = new ReferenceType(); // Instanciando a classe ReferenceType para testar os tipos de referência
-           // referenceType.TestsTypes();
-           //
-           // ValueTypes valueTypes = new ValueTypes(); // Instanciando a classe ValueTypes para testar os tipos primitivos
-           // valueTypes.TestsTypes();
-           //
-           // UsingExample.Test();
-           //
-           // Operators salaryCalculator = new Operators(5000.00m, 15, 8); // Instanciando a classe Operators para calcular o salário líquido
-           // salaryCalculator.CalculateNetSalary();
-           //
-           //  // Tests contrutores
-           //
-           //    Product product = new Product("Laptop", 1500.00, 10); // Instanciando a classe Product para testar os construtores
-           //    
-           //    var movie = new Movie("Batman", "Animação"); // Instanciando a classe Movie para testar os construtores
-           //    Console.WriteLine(movie.Title);
-           //    
-           //    Users.DestructorExample destructorExample = new Users.DestructorExample(); // Instanciando a classe DestructorExample para testar os destrutores
-           //    destructorExample.ShowMessage(); // Exibindo o nome do usuário
-           //  
-           //    // Tests conditionals:
-           //    
-           //      ConditionalSimple conditionalSimple = new ConditionalSimple(); // Instanciando a classe ConditionalSimple para testar os condicionais
-           //      conditionalSimple.CheckAge(); // Verificando a idade do usuário
-           //      conditionalSimple.CheckAge2(); // Verificando a idade do usuário com else
-           //      conditionalSimple.CheckAge3(); // Verificando a idade do usuário com else if
-           //      conditionalSimple.CheckAge4(); // Verificando a idade do usuário com switch
-           
-           // Tests ChallangeWeekly01:
-           // var consoleView = new ConsoleView();
-           // while (true)
-           // {
-           //     consoleView.ShowMenu();
-           //     consoleView.Run();
-           // }
-           
+            Exceptions exceptions = new Exceptions();
+            exceptions.Convert_Int();
         }
     }
 }
